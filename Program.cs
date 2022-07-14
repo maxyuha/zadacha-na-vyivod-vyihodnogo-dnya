240108f472c9a57894c870a63b4bf928597820c0
Maksim <maxyuha1998@gmail.com>
2022-07-14 16:21:39 +0300

    make task2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+        void Domashka()
+        {
+            Console.WriteLine("введите номер дня недели от 1 до 7");
+            int number=Convert.ToInt32(Console.ReadLine());
+            
+            if (number==6||number==7)
+            {
+                Console.WriteLine("Это выходной день");
+            }
+            
+            else if (1>number||7<number)
+                Console.WriteLine("Ошибка ввода");
+
+            else Console.WriteLine("рабочий день");
+            
+
+            
+            
+        }
+        Domashka();
+     }
+    
+ }
+} 
